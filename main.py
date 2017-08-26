5b88248dea2b4fcc22e10a948ad54c4102c2deca
haohaoh4 <handahao@163.com>
2017-08-26 20:46:42 +0800

    add get_click()

---
+++ main.py
@@ -0,0 +1,20 @@
+import click
+import time
+
+def main(f,to):
+    print "start"
+    max_click = -1
+    max_id = -1
+    for i in range(f, to):
+        current_click = click.get_click(i)
+        if current_click > max_click:
+            max_click = current_click
+            max_id = i
+        if(i % 5 ==0 and i != to - 1):
+            print "%s%%" % (100.0 * (i - f) / (to - f))
+            print "temp max:av%s\tclicks:%s" % (max_id, max_click)
+        time.sleep(0.2)
+    print "100.0%%"
+    return "max:av%s\tclicks:%s" % (max_id, max_click)
+
+print main(10000,100000)
